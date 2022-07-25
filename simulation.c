b1f8c7ca32c91848290971f0df60c5994226e90a
kate20031 <83357098+kate20031@users.noreply.github.com>
2022-07-25 17:18:45 +0200

    Zamiana bool* visited  na charArray* visited

--- simulation.c
+++ simulation.c
@@ -12,7 +12,7 @@
  * @param dimSize – size of "dimensions" array.
  * @return volume of maze.
  */
-size_t findVolume(Array* dimensionArray) {
+size_t findVolume(Array *dimensionArray) {
     size_t dimSize = getLength(dimensionArray);
     size_t volume = (dimSize == 0) ? 0 : 1;
     for (size_t i = 0; i < dimSize; i++) {
@@ -59,9 +59,8 @@ void convertIndexRev(size_t index, size_t *coordinatesArray, Array *dimensionArr
  * @return - true, if it's safe to go to the "aim" cell,
  * false - otherwise,
  */
-static bool isSafe(size_t mazeDimension, charArray* bitPositions, const bool* visited, size_t aim)
-{
-    return (aim < mazeDimension) && (getBit(bitPositions, aim) == 0) && (visited[aim] == false);
+static bool isSafe(size_t mazeDimension, charArray *bitPositions, charArray *visited, size_t aim) {
+    return (aim < mazeDimension) && (getBit(bitPositions, aim) == 0) && (getBit(visited, aim) == 0);
 }
 
 
@@ -75,15 +74,15 @@ static bool isSafe(size_t mazeDimension, charArray* bitPositions, const bool* vi
  * @return - true, if the cell is out of border,
  * false - otherwise.
  */
-static bool outMazeBorder(Array* dimensionsArray, size_t cell, long  long neighbor, size_t index) {
-    size_t* coordinates = malloc (sizeof(size_t) * getLength(dimensionsArray));
+static bool outMazeBorder(Array *dimensionsArray, size_t cell, long long neighbor, size_t index) {
+    size_t *coordinates = malloc(sizeof(size_t) * getLength(dimensionsArray));
     convertIndexRev(cell, coordinates, dimensionsArray);
 
-    if ((coordinates[index] == getElementFromArray(dimensionsArray, index)) && neighbor  > 0) {
+    if ((coordinates[index] == getElementFromArray(dimensionsArray, index)) && neighbor > 0) {
         free(coordinates);
         return true;
     }
-    if (coordinates[index] == 1 && neighbor  < 0) {
+    if (coordinates[index] == 1 && neighbor < 0) {
         free(coordinates);
         return true;
     }
@@ -104,10 +103,10 @@ static bool outMazeBorder(Array* dimensionsArray, size_t cell, long  long neighb
  * @return the length of shortest path, if exists,
  *          -1 – otherwise.
  */
-size_t findPath (charArray* bitPositions, Array* dimensionsArray, size_t start, size_t end) {
+size_t findPath(charArray *bitPositions, Array *dimensionsArray, size_t start, size_t end) {
     size_t dimSize = getLength(dimensionsArray);
     size_t volume = findVolume(dimensionsArray);
-    bool* visited = malloc(sizeof (bool) * volume);
+    charArray *visited = createArrayChar();
 
     if (visited == NULL) {
         handleError(0);
@@ -115,13 +114,13 @@ size_t findPath (charArray* bitPositions, Array* dimensionsArray, size_t start, 
     }
     size_t minDistance = INT64_MAX;
 
-    for (size_t i = 0; i < volume; i++) {
-        visited[i] = false;
+    for (size_t i = 0; i < volume / 4; i++) {
+        pushBackChar(visited, '0');
     }
 
     queue *q;
     q = queueCreate();
-    visited[start] = true;
+    setBit(visited, end);
     addToQueue(q, start, 0);
 
     while (!queueEmpty(q)) {
@@ -140,12 +139,12 @@ size_t findPath (charArray* bitPositions, Array* dimensionsArray, size_t start, 
 
             if (isSafe(volume, bitPositions, visited, index + neighbor) &&
                 !outMazeBorder(dimensionsArray, index, neighbor, i)) {
-                visited[index + neighbor] = true;
+                setBit(visited, index + neighbor);
                 addToQueue(q, index + neighbor, distance + 1);
             }
             if (isSafe(volume, bitPositions, visited, index - neighbor) &&
                 !outMazeBorder(dimensionsArray, index, -1 * neighbor, i)) {
-                visited[index - neighbor] = true;
+                setBit(visited, index - neighbor);
                 addToQueue(q, index - neighbor, distance + 1);
             }
             neighbor *= getElementFromArray(dimensionsArray, i);
